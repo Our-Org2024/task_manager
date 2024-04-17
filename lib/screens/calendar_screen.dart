8c257b78b5f01579ee6172aae7b33a928ccefce7
khaled-hassan1 <khaled.ghaly303@gmail.com>
2024-04-17 19:54:11 +0200

    some improv

--- lib/screens/calendar_screen.dart
+++ lib/screens/calendar_screen.dart
@@ -5,14 +5,24 @@ class CalendarScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Calendar',
-        style: Theme.of(context)
-            .textTheme
-            .bodyLarge!
-            .copyWith(color: Colors.black),
-      ),
+    return Column(
+      children: [
+        CalendarDatePicker(
+          firstDate: DateTime(1950-1-1),
+          onDateChanged: (DateTime value) {},
+          lastDate:DateTime.now() ,
+          initialDate: null,
+        ),
+        Center(
+          child: Text(
+            'Calendar',
+            style: Theme.of(context)
+                .textTheme
+                .bodyLarge!
+                .copyWith(color: Colors.black),
+          ),
+        ),
+      ],
     );
   }
 }
